43f970cc84aa65bf8e5ef09d1c39b171a245dc05
fis007 <schaffhauserfilip@gmail.com>
2020-12-04 18:22:54 +0800

    2:58:58

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -5,6 +5,12 @@ import "./SearchPage.css";
 import Response from "../response";
 import { Link } from "react-router-dom";
 import Search from "../components/Search";
+import SearchIcon from "@material-ui/icons/Search";
+import DescriptionIcon from "@material-ui/icons/Description";
+import ImageIcon from "@material-ui/icons/Image";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import RoomIcon from "@material-ui/icons/Room";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
@@ -26,12 +32,60 @@ function SearchPage() {
 
         <div className="searchPage__headerBody">
           <Search hideButtons />
+
+          <div className="searchPage__options">
+            <div className="searchPage__optionsLeft">
+              <div className="searchPage__option">
+                <SearchIcon />
+                <Link to="/all">All</Link>
+              </div>
+              <div className="searchPage__option">
+                <DescriptionIcon />
+                <Link to="/news">News</Link>
+              </div>
+              <div className="searchPage__option">
+                <ImageIcon />
+                <Link to="/images">Images</Link>
+              </div>
+              <div className="searchPage__option">
+                <LocalOfferIcon />
+                <Link to="/shopping">shopping</Link>
+              </div>
+              <div className="searchPage__option">
+                <RoomIcon />
+                <Link to="/maps">maps</Link>
+              </div>
+              <div className="searchPage__option">
+                <MoreVertIcon />
+                <Link to="/more">more</Link>
+              </div>
+            </div>
+
+            <div className="searchPage__optionsRight">
+              <div className="searchPage__option">
+                <Link to="/settings">Settings</Link>
+              </div>
+              <div className="searchPage__option">
+                <Link to="/tools">Tools</Link>
+              </div>
+            </div>
+          </div>
         </div>
-
-        <h1>{term}</h1>
       </div>
+      {true && <div className="searchPage__results"></div>}
 
-      <div className="searchPage__results"></div>
+      <div className="searchPage__results">
+        <p className="searchPage__resultCount">
+          About {data?.searchInformation.formattedTotalResults} results (
+          {data?.searchInformation.formattedSearchTime} seconds) for {term}
+        </p>
+
+        {data?.items.map((item) => (
+          <div className="searchPage__result">
+            <a href={item.link}>{item.displayLink} ▽</a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
