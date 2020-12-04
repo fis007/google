d72183631bf239b27ff745b80edbcf9fa2a40c9b
fis007 <schaffhauserfilip@gmail.com>
2020-12-04 17:09:13 +0800

    test

---
+++ src/keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = "AIzaSyA-00BqdBoWo64phBp-s6olTvouvswQvQg";
+
+export default API_KEY;
---
+++ src/pages/SearchPage.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useStateValue } from "../StateProvider";
+import useGoogleSearch from "../useGoogleSearch";
+import "./SearchPage.css";
+import Response from "../response";
+import { Link } from "react-router-dom";
+import Search from "../components/Search";
+
+function SearchPage() {
+  const [{ term }, dispatch] = useStateValue();
+
+  //   const { data } = useGoogleSearch(term);
+
+  const data = Response;
+
+  console.log(data);
+  return (
+    <div className="searchPage">
+      <div className="searchPage__header">
+        <Link to="/">
+          <img
+            className="searchPage__logo"
+            src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          />
+        </Link>
+
+        <div className="searchPage__headerBody">
+          <Search hideButtons />
+        </div>
+
+        <h1>{term}</h1>
+      </div>
+
+      <div className="searchPage__results"></div>
+    </div>
+  );
+}
+
+export default SearchPage;
